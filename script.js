026246abb9ee34c1a2431b4a4fbf3b98f0573154
Anderson0240 <80935525+Anderson0240@users.noreply.github.com>
2025-05-27 15:08:22 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const parallaxBg = document.getElementById("parallax-bg");
-  
-    window.addEventListener("scroll", function () {
-      let scrollY = window.scrollY;
-      parallaxBg.style.transform = `translateY(${scrollY * 0.5}px)`;
-    });
-  });
-  
